54b2f510e0ea024b59e85cb7b6c3bf39d4504489
Yomikoda <yomiko@Yomiko-Padalecki.local>
2016-10-31 11:15:00 +0100

    Beginning of aside section nav +  broken JS that goes with it to fix later

--- js/overview.js
+++ js/overview.js
@@ -7,6 +7,30 @@ $(function(){
                                 });
         
 });
+   
+    
+    /** FIGURE OUT WHY ITS NOT WORKING -__- **/
+    $(window).scroll(function(){
+         valeurDuScroll=$(window).scrollTop();
+              
+        /** SMOOTH SCROLL ( ANCHORS + NAV ) **/
+    
+     $('#side>nav>ul>li:first-child').on('click',function(){
+        $('html,body').stop().animate({scrollTop:$('#introduction').offset().top-80},1000);
+        return false;
+    });
+    $('#side>nav>ul>li:nth-child(2)').on('click',function(){
+        $('html,body').stop().animate({scrollTop:$('#splash').offset().top-80},1500);
+        return false;
+    });
+
+        
+        
+        
+     });
+    
+    
+    
     
     
     
