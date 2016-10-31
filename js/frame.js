9d25f2387131f38226cf9076640e1fb5267b3ad2
Yomikoda <yomiko@Yomiko-Padalecki.local>
2016-10-31 18:07:01 +0100

    final correction on frame.js menu-wise

--- js/frame.js
+++ js/frame.js
@@ -7,13 +7,8 @@ $(function(){
       
          
     if(valeurDuScroll>30){
-       $('header').css({'opacity':'0.3'});          
-    }
-         
-    else{
-        $('header').css({'opacity':'1'}); 
-    
-     $('header').mouseover(
+       $('header').css({'opacity':'0.3'}); 
+         $('header').mouseover(
         function() {
     $('header').css({'opacity':'1'});        
         }
@@ -21,6 +16,17 @@ $(function(){
          $('header').mouseout(
         function() {
     $('header').css({'opacity':'0.3'});        
+        }
+    );  
+    }
+         
+    else{
+        $('header').css({'opacity':'1'}); 
+    
+        
+         $('header').mouseout(
+        function() {
+    $('header').css({'opacity':'1'});        
         }
     ); 
     
