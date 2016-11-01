9a378a1e6650a324eddf61ac834e547b5f5aedb2
oliver bsaibes <oliverbsaibes@gmail.com>
2016-11-01 17:43:16 +0100

    creating js function

---
+++ js/affmas.js
@@ -0,0 +1,39 @@
+     function AfficherMasquer() {
+         divInfo = document.getElementById('simp');
+
+         if (divInfo.style.display == 'none')
+             divInfo.style.display = 'block';
+         else
+             divInfo.style.display = 'none';
+
+     }
+
+     function Masquer() {
+         divInfo = document.getElementById('simp');
+
+         if (divInfo.style.display == 'block')
+             divInfo.style.display = 'none';
+         else
+             divInfo.style.display = 'block';
+
+     }
+
+     function Afficherbloc1() {
+         divInfo = document.getElementById('performance');
+
+         if (divInfo.style.display == 'none')
+             divInfo.style.display = 'block';
+         else
+             divInfo.style.display = 'none';
+
+     }
+
+     function Masquerb1() {
+         divInfo = document.getElementById('performance');
+
+         if (divInfo.style.display == 'block')
+             divInfo.style.display = 'none';
+         else
+             divInfo.style.display = 'block';
+
+     }
