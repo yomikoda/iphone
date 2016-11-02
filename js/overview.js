092faac8f27fc9f2a2b6641e4602547b1e146d11
Yomikoda <yomiko@Yomiko-Padalecki.local>
2016-11-02 22:33:49 +0100

    Minor Css tinkering - Sticky-shopping bag added ( to be transfered to all pages )+ css of sticky

--- js/overview.js
+++ js/overview.js
@@ -1,7 +1,7 @@
 $(function(){
     
     $( document ).ready(function() {
-        $('#leftblock h1').css({'opacity':'1','top':'80px',function(){
+        $('#leftblock h1').css({'opacity':'1','top':'130px',function(){
             $('#introiphone').css({'opacity':'1','bottom':'80px'});
                                     }
                                 });
